a63cefbdb31adb13ff3df82dcbb6d6339b26ec50
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-02-09 16:00:02 -0500

    WEB39 TODO APP

--- src/App.js
+++ src/App.js
@@ -13,10 +13,26 @@ class App extends React.Component {
   };
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  handleToggle = (itemId) => {
+    this.setState({
+      todo: this.state.todos.map((item) => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      }),
+    });
+  };
+
+  // handleAddingTask = (task) => {}
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Welcome to My Todo App!</h2>
       </div>
     );
   }
